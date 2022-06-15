ff93d7c3f161d30d6ca05edeb4faefa5cb6e6db7
Maja Michaelis <maja.michaelis@study.hs-duesseldorf.de>
2022-06-15 15:25:50 +0200

    neues Projekt yay

---
+++ Assets/FishAgent.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishAgent : MonoBehaviour
+{
+    public SwarmManager swarmManager;
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(swarmManager.minSpeed, swarmManager.maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, Time.deltaTime * speed);
+        ApplyRules();
+    }
+
+    private void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = swarmManager.allfish;
+
+        Vector3 vcentre = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.01f;
+        float nDistance;
+        int groupSize = 0;
+
+        foreach(GameObject go in gos)
+        {
+            if(go != this.gameObject)
+            {
+                nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+
+                //nur für Fische, die sich in der Nachbarschaft befinden
+                if(nDistance <= swarmManager.neighbourDistance)
+                {
+                    //eine Untergruppe, dessen Positionen werden berücksichtigt 
+                    vcentre += go.transform.position;
+                    groupSize++;
+
+                    if(nDistance < swarmManager.AvoidValue)
+                    {
+                        //wegbewegen 
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+
+                    FishAgent anotherFish = go.GetComponent<FishAgent>();
+                    gSpeed = gSpeed + anotherFish.speed;
+                }
+            }
+        }
+        if (groupSize > 0)
+        {
+            vcentre = vcentre / groupSize;
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+
+            //schon Richtige Richtung 
+            if (direction != Vector3.zero)
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), swarmManager.rotationSpeed * Time.deltaTime);
+
+
+        }
+    }
+}
